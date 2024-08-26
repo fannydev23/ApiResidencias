4c654e243601d2d4e3d98e6a22ab55f734ff65e9
Estefania <estefaniapichardo1999@gmail.com>
2024-08-26 13:49:32 -0600

    change connection string

--- ApiResidencias/Models/Entities/residenciasContext.cs
+++ ApiResidencias/Models/Entities/residenciasContext.cs
@@ -33,7 +33,7 @@ public partial class residenciasContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=sistemas19.com;user=sistem21_residenciasDB;password=sistemas19_;database=sistem21_residenciasDB;treattinyasboolean=true", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.5.20-mariadb"));
+        => optionsBuilder.UseMySql("server=myserver;user=myuser;password=mypassword;database=myDB;treattinyasboolean=true", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.5.20-mariadb"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
